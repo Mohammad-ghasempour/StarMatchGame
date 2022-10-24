db0de9c76a0f68de50ea7b5207f74c198ebe76c9
Mohammad <mohammad.ghasempour@bouvet.no>
2022-10-24 09:59:40 +0200

    build preparasions of stars and numbers

---
+++ star-match-game/src/App.js
@@ -0,0 +1,11 @@
+import StarGame from './StarGame';
+
+function App() {
+  return (
+    <div >
+      <StarGame/>
+           </div>
+  );
+}
+
+export default App;
---
+++ star-match-game/src/StarGame.js
@@ -0,0 +1,33 @@
+import "./Style.css";
+import {utils , colors}  from './utils'
+import React , { useState } from "react";
+const StarGame = () => {
+  const [stars, setStars] = useState(utils.random(1,9));
+
+  return (
+    <div className="App">
+      <div className="game">
+        <div className="help">
+          Pick 1 or more numbers that sum to the number of stars
+        </div>
+        <div className="body">
+          <div className="left">
+            {utils.range(1, stars).map((starId) => (
+              <div key={starId} className="star" />
+            ))}
+          </div>
+          <div className="right">
+            {utils.range(1, 9).map((number) => (
+              <button key={number} className="number">
+                {number}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="timer">Time Remaining: 10</div>
+      </div>
+    </div>
+  );
+};
+
+export default StarGame;
